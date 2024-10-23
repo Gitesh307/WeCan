8c7516a0f44caa25d8292d518a1217ddb0c677f1
ahearndylan <dahearn2021@gmail.com>
2024-10-23 18:02:34 -0400

    Update base and add new templates

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
 from homepage import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path("homepage/", views.home, name='homepage'),
+    path("", views.home, name='homepage'),
+    path('schedule/', views.schedule, name='schedule'), 
+    path('services/', views.services, name='services'), 
+    path('contact/', views.contact, name='contact'), 
+    path('resources/', views.resources, name='resources'), 
+    path('works/', views.works, name='works'), 
+    path('who/', views.who, name='who'), 
+    path('policy/', views.policy, name='policy'), 
+    path('terms/', views.terms, name='terms'), 
+    path('blog/', views.blog, name='blog'), 
+    path('faq/', views.faq, name='faq'), 
+    path('gallery/', views.gallery, name='gallery'), 
+    path('volunteers/', views.volunteers, name='volunteers'), 
+    path('register/', views.register, name='register'), 
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='homepage'), name='logout'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html", {})
+
+def schedule(request):
+    return render(request, 'schedule.html') 
+
+def services(request):
+    return render(request, 'services.html') 
+
+def contact(request):
+    return render(request, 'contact.html') 
+
+def resources(request):
+    return render(request, 'resources.html') 
+
+def works(request):
+    return render(request, 'works.html') 
+
+def who(request):
+    return render(request, 'who.html') 
+
+def policy(request):
+    return render(request, 'policy.html') 
+
+def terms(request):
+    return render(request, 'terms.html') 
+
+def blog(request):
+    return render(request, 'blog.html') 
+
+def faq(request):
+    return render(request, 'faq.html') 
+
+def gallery(request):
+    return render(request, 'gallery.html') 
+
+def volunteers(request):
+    return render(request, 'volunteers.html') 
+
+def register(request):
+   if request.method == 'POST':
+       form = UserRegistrationForm(request.POST)
+       if form.is_valid():
+           form.save()
+           messages.success(request, f'Your account has been created. You can log in now!')   
+           return redirect('login')
+   else:
+       form = UserRegistrationForm()
+   context = {'form': form}
+   return render(request, 'register.html', context)
+
+def login_view(request):
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
