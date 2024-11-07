12651e79b09cf70dcd2bbb24e53d5b3a21f500ff
Gitesh Sagvekar <128126970+Gitesh307@users.noreply.github.com>
2024-11-07 00:15:54 -0500

    Created Subscriber profile page and grid cards for homepage

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,1 +1,8 @@
 # Register your models here.
+from django.contrib import admin
+from .models import Subscriber, RecyclingHistory, AccountBalance
+
+# Register the models
+admin.site.register(Subscriber)
+admin.site.register(RecyclingHistory)
+admin.site.register(AccountBalance)
